4bb697795db13fc6ccaf327a16dbde815ecca401
hiwonee <zgz20515@gmail.com>
2022-07-28 17:41:31 +0900

    d

--- 004.jQuery학습/003.응용구현/shoppingMall_PJ/js/main3.js
+++ 004.jQuery학습/003.응용구현/shoppingMall_PJ/js/main3.js
@@ -6,6 +6,7 @@
         // 0. 호출확인
         console.log("로딩완!");
 
+
         /*************************************************************** 
         [페이드 배너 요구사항] 
 
@@ -23,9 +24,9 @@
         // 이벤트 대상: .abtn
         // 이벤트 : click() 메서드 사용
         // 양쪽 버튼 구분 : .ab1(왼쪽) / .ab2(오른쪽버튼)
-        // 변경대상: #slide
+        // 변경대상: #slide li
         // 변경내용: slide의 left값을 이동하여 애니메이션 함
-        let slide = $("#slide");
+        let slide = $("#slide li"); // li까지 선택함
 
         // 변경에 사용할 제이쿼리 메서드 : animate({CSS 속성},시간,이징,함수)
         
@@ -43,6 +44,17 @@
         // 애니메이션 이징 변수
         const aniE = "easeOutQuint";
 
+        // 페이드 기능을 위한 초기화 설정
+        // 슬라이드 li가 하나만 빼고 모두 불필요!!
+        // display:none 처리함!! -> hide()메서드
+        // slide.hide()  #slide li를 slide로 묶어줬잖아
+        slide.hide().first().show();
+        // 슬라이드li.다숨겨().첫번째().보여()
+
+        // 현재 슬라이드 순번 변수
+        let sno = 0; 
+
+        // 버튼 클릭 설정
         $(".abtn").click(function(){
             // console.log("진입",prot);
             
@@ -54,7 +66,7 @@
             // console.log("통과",prot);
 
             // 자동 넘김 지우기 함수 호출!
-            // clearAuto();
+            clearAuto();
 
             // 1. 오른쪽 여부
             // is(클래스/아이디명)-> 선택요소의 해당여부 리턴
@@ -68,6 +80,18 @@
 
             if(isR){// 오른쪽 버튼
                 
+                // fadeIn으로 다음 순번 보이기
+                // eq(순번) - sequance,,, eq -> 해당 순번 선택!
+
+                slide.eq(sno).fadeOut(aniT,aniE); // 현재 슬라이드 숨기기
+
+                sno++; // 슬라이드 순번 1 증가
+
+                // slide.eq().fadeIn(aniT,aniE); 
+                // 분기
+                if(sno===slide.length) sno=0; // 슬라이드 한계값 체크 처음으로 변경! 한계값이냐? 그럼 처음으로 돌아가
+                console.log("현재슬번:",sno); // slide.length는 li개수
+                slide.eq(sno).fadeIn(aniT,aniE); // 다음 순번 슬라이드 나타남
 
                 
 
@@ -75,12 +99,29 @@
 
             else {///// 왼쪽 버튼
 
-      
+                // fadeIn으로 이전 순번 보이기
+                // eq(순번) -> 해당 순번 선택
+
+                slide.eq(sno).fadeOut(aniT,aniE); // 현재 슬라이드 숨기기
+
+                sno--; // 슬라이드 순번 1 감소
+
+                // slide.eq().fadeIn(aniT,aniE); 
+                // 분기
+                if(sno===-1) sno=slide.length-1; 
+                // 슬라이드 한계값 체크 처음으로 변경! 한계값이냐? 그럼 처음으로 돌아가
+                console.log("현재슬번:",sno); // slide.length는 li개수
+                slide.eq(sno).fadeIn(aniT,aniE); // 다음 순번 슬라이드 나타남
                 
             }///////////////////// else ///////////////////////
 
-            // 블릿 순번 표시 변경하기
+            
             // 3. 등장 슬라이드와 같은 순번의 블릿변경하기
+            // 현재슬라이드번호(sno)와 같은 순번의 블릿 클래스 on
+            indic.eq(sno).addClass("on")
+            // 다른 형제들 블릿 클래스제거
+            .siblings().removeClass("on");
+
             
         }); /////////// click ///////////////////////////
 
@@ -119,13 +160,31 @@
         let autoT;
 
         // 인터발 최초호출!
-        // autoSlide();
+        autoSlide();
 
         // 인터발 호출 함수
         function autoSlide(){
             autoI = setInterval(()=>{
                 
-        
+                    // fadeIn으로 다음 순번 보이기
+                    // eq(순번) - sequance,,, eq -> 해당 순번 선택!
+    
+                    slide.eq(sno).fadeOut(aniT); // 현재 슬라이드 숨기기
+    
+                    sno++; // 슬라이드 순번 1 증가
+    
+                    // slide.eq().fadeIn(aniT); 
+                    // 분기
+                    if(sno===slide.length) sno=0; // 슬라이드 한계값 체크 처음으로 변경! 한계값이냐? 그럼 처음으로 돌아가
+                    console.log("현재슬번:",sno); // slide.length는 li개수
+                    slide.eq(sno).fadeIn(aniT); // 다음 순번 슬라이드 나타남
+    
+                    
+                // 3. 등장 슬라이드와 같은 순번의 블릿변경하기
+                // 현재슬라이드번호(sno)와 같은 순번의 블릿 클래스 on
+                indic.eq(sno).addClass("on")
+                // 다른 형제들 블릿 클래스제거
+                .siblings().removeClass("on");
                        
             }, 3000);
         } /////////////////// autoSlide함수 ////////////////////////
@@ -136,7 +195,7 @@
             clearInterval(autoI);
 
             // 일정 시간후 다시 인터발 호출 (다시 넘어가게 해줘야할거아냐)
-            /* setTimeout(autoSlide, 4000); */ /* 이러면 4초 있다가 또 3초 뒤에 움직이겠지 */
+            // setTimeout(autoSlide, 4000); /* 이러면 4초 있다가 또 3초 뒤에 움직이겠지 */
             // 이러고 클릭 광클 해봐 setTimeout쓰나미 옴 우르르 넘어갘ㅋㅋㅋㅋㅋㅋ
 
             // 타임아웃지우기(실행쓰나미방지!)
