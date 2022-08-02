d1e9d4e258231ddf55fbf810de2da600720756ea
hiwonee <zgz20515@gmail.com>
2022-08-02 20:24:47 +0900

    d

--- 001.HTML 학습/08.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML 학습/08.VOGUE_PJ/002.site/js/common.js
@@ -114,11 +114,11 @@ $(()=>{
             }); ////// each ////////////////////
 
             // 위치 배열 값 확인!
-            console.log(scpos);
+            // console.log(scpos);
         });/////////// resize ////////
 
         $(window).on("mousewheel wheel",()=>{
-            console.log("ㅎㅎㅎ");
+            // console.log("ㅎㅎㅎ");
             // 부드러운 스크롤 위치 변수 pos 업데이트
             // pos = scTop;
         })
@@ -239,9 +239,109 @@ $(()=>{
 
 
 // 윈도우 객체 컨텐츠가 모두 로딩된 후 로딩박스 없애기
-$(window).on("load",function(){
-    $(".LDbx").hide();
-}); ///////////////////////// load //////////////////
+// $(window).on("load",function(){
+//     $(".LDbx").hide();
+// }); ///////////////////////// load //////////////////
 
 // 스크롤 등장 요소 (.scAct)만 위치값을  배열에 저장 더짧게~
 // $(window).resize(()=>{scAct.each((idx,ele)=>scpos[idx]=$(ele).offset().top); });
+
+/****************************************************************** 
+    제이쿼리 스크롤 멈춤 체크 플러그인 메서드
+******************************************************************/
+// $.fn.scrollStopped = function(){
+//     // 위치값 부드러운 스크롤에 주려고
+    
+// }; /////////// 스크롤 멈춤 플러그인 ///////////////////////
+$.fn.scrollStopped = function(callback){
+    // 위치값 부드러운 스크롤에 주려고
+
+    // 스크롤 이벤트 설정하기
+    $(this).scroll(function(){
+        
+        //1. 먼저 타임아웃 지우기 (아래서 설정된 타임아웃,, 쓰나미 현상방지)
+        clearTimeout($(this).data('scTime'));
+        //2. 타임아웃 설정을 한다!
+        $(this).data('scTime', setTimeout(callback,152))
+        // $(this).data('scrollTimeout') data값을 설정, data
+        // sctime은 데이타 이름, setTimeout(함수, 시간)저 함수를 정해준 시간 뒤에 실행
+    
+        // 제이쿼리 data() 메서드
+        // -> data(키,값) 특정이름에 값을 세팅하여 저장!
+    
+        // 타임아웃의 시간은  부드러운 스크롤에 셋팅된 시간인 speed, smooth 시간을 더한 값으로 넣어준다.. new SmoothScroll(document, 120, 32)으로 smoothScroll20.js에 있거든,,, 그래서 120+32 -> 152는 저 콜백 뒤에 써주자
+
+    }); //// 스크롤시 계속 발생구역//////
+    
+
+}; /////////// 스크롤 멈춤 플러그인 ///////////////////////
+
+/// 윈도우 객체를 제이쿼리로 플러그인 호출!!
+// 이때 실행될 함수를 전달함!!
+// 플러그인에 함수 전달,,
+// 플러그인에 콜백을 받는게 함수
+$(window).scrollStopped(function(){
+    // 마지막 스크롤 위치값
+    let scTop = $(this).scrollTop();
+
+    // 콘솔에 확인!
+    // console.log("스크롤멈춤!",scTop);
+    // 부드러운 스크롤 위치값에 업데이트!
+    pos = scTop;
+
+});//////////////////// window //////////////////////////////
+
+$(()=>{
+    // 제이쿼리 data(키,값)연습
+    let mykey = $("#top");
+    mykey.data('헐','고양이');
+    mykey.data('힘','강아지');
+
+    // console.log('data()키 값:',mykey.data('헐'),'/',mykey.data('힘'));
+
+    //data() 삭제 방법은
+    mykey.removeData('헐');
+
+    // console.log (
+    //     'data(키)값'
+    //     ,mykey.data('헐'),'/',mykey.data('힘')
+    // )
+    // 태그 안에 data-mykey="하하하" 이런 코딩법이 있쥬
+
+}); ///////////// jQB /////////////////////////////
+
+
+
+//////////////////////// 비교 JS Map() 메서드 ///////////////////////////
+// - 키, 값으로 저장하는 메모리 공간 생성 메서드 //////////////////
+// 1. 메모리 공간 생성: Map() -> new 키워드로!
+// Map() 객체 아래 메서드: set(),get(),has(),delete()
+
+
+const keyval = new Map();
+
+// 2. 값 셋팅: set(키, 값)
+keyval.set('한국배우','조승우');
+keyval.set('미국배우','라이언');
+keyval.set('중국배우','여명');
+
+// 3. 값 호출: get(키)
+// console.log('Map()값:',
+// keyval.get('한국배우'),'/',
+// keyval.get('미국배우'),'/',
+// keyval.get('중국배우')
+// );
+
+// 4. 키 존재 여부: has(키)
+// console.log('여명있나 지금?', keyval.has('중국배우'));
+
+// 5. 값 삭제: delete(키)
+keyval.delete('중국배우');
+
+// console.log('여명있나 지금?', keyval.has('중국배우'));
+
+// console.log('Map()값:',
+// keyval.get('한국배우'),'/',
+// keyval.get('미국배우'),'/',
+// keyval.get('중국배우')
+// );
